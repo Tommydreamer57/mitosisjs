b54c8aaee98db46c630461eaa2dc001b9bc39776
Tommydreamer57 <minilao94@yahoo.com>
2018-07-03 11:16:30 -0600

    created package

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const mitosis = (createApp, model, render, ...middles) => {
+
+    var middlewares, app, update;
+
+    update = (callback, cb) => {
+
+        let newModel = callback(model);
+
+        if (newModel) {
+            if (middlewares) for (let fn of middlewares) newModel = fn(newModel);
+
+            model = newModel;
+
+            if (app) render(app.view(model), cb);
+        }
+    }
+
+    update.access = key => key ?
+        Array.isArray(key) ?
+            key.reduce((m, k) => m[k], model)
+            :
+            model[key]
+        :
+        model;
+
+    middlewares = middles.map(fn => fn(update));
+
+    app = createApp(update);
+
+    update(m => m);
+}
+
+exports.default = mitosis;
